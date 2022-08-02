c82adf1d1027cd9296d20139aa4db56f113f153c
Ali Rustamov <ali.a.rustamov@gmail.com>
2022-08-03 02:57:58 +0600

    use webpack-merge

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,37 +1,6 @@
-const path = require("path");
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
 
-module.exports = function(_env, argv) {
-
-    return {
-        mode: 'production',
-        entry: "./src/index.tsx", // входная точка - исходный файл
-        output:{
-            path: path.resolve(__dirname, "./public"),     // путь к каталогу выходных файлов - папка public
-            publicPath: "/public/",
-            filename: "bundle.js"       // название создаваемого файла
-        },
-        devServer: {
-            historyApiFallback: true,
-            static: {
-                directory: path.join(__dirname, "/"),
-            },
-            port: 8081,
-            open: true
-        },
-        module:{
-            rules:[   //загрузчик для jsx
-                {
-                    test: /\.(js|jsx|tsx|ts)$/, // определяем тип файлов
-                    exclude: /(node_modules)/,  // исключаем из обработки папку node_modules
-                    loader: "babel-loader",   // определяем загрузчик
-                    options:{
-                        presets:[ "@babel/preset-react", "@babel/preset-typescript"]    // используемые плагины
-                    }
-                }
-            ],
-        },
-        resolve: {
-            extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
-        }
-    };
-}
+module.exports = merge(common, {
+    mode: 'production',
+})
