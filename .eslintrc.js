d53989cf491e2b98ccaca30673ebd1a3f956f900
Ali Rustamov <ali.a.rustamov@gmail.com>
2022-08-04 11:20:52 +0600

    feat: add redux

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,45 +1,53 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'plugin:prettier/recommended',
+    'prettier/prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: [
-        'plugin:react/recommended',
-        'airbnb',
-        'airbnb-typescript',
-        'plugin:prettier/recommended',
-        'prettier/prettier',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.eslint.json',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  rules: {
+    'import/prefer-default-export': 'off',
+    'import/extensions': 'off',
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+      },
     ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        project: './tsconfig.eslint.json',
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    'react/jsx-props-no-spreading': 'off',
+  },
+  overrides: [
+    {
+      files: ['src/**/*Slice.ts'],
+      rules: {
+        'no-param-reassign': 'off',
+      },
     },
-    plugins: ['react', '@typescript-eslint'],
-    settings: {
-        'import/resolver': {
-            node: {
-                extensions: ['.js', '.jsx', '.ts', '.tsx'],
-            },
-        },
-    },
-    rules: {
-        'import/prefer-default-export': 'off',
-        'import/extensions': 'off',
-        'react/function-component-definition': [
-            2,
-            {
-                namedComponents: 'arrow-function',
-            },
-        ],
-        'react/jsx-filename-extension': [
-            2,
-            { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
-        ],
-        'react/jsx-props-no-spreading': 'off',
-    },
-}
+  ],
+};
--- src/components/pages/Main.tsx
+++ src/components/pages/Main.tsx
@@ -1,7 +1,57 @@
 import React, { FC } from 'react';
+import { Counter } from '../../features/counter/Counter';
 
-export const Main: FC = () => (
-  <div>
-    <h1>Hello!</h1> This is the main page
-  </div>
-);
+export const Main: FC = () => {
+  return (
+    <div>
+      <h1>Hello!</h1> This is the main page
+      <div className="App">
+        <header className="App-header">
+          <img src={`${''}/logo.svg`} className="App-logo" alt="logo" />
+          <Counter />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <span>
+            <span>Learn </span>
+            <a
+              className="App-link"
+              href="https://reactjs.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              React
+            </a>
+            <span>, </span>
+            <a
+              className="App-link"
+              href="https://redux.js.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Redux
+            </a>
+            <span>, </span>
+            <a
+              className="App-link"
+              href="https://redux-toolkit.js.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Redux Toolkit
+            </a>
+            ,<span> and </span>
+            <a
+              className="App-link"
+              href="https://react-redux.js.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              React Redux
+            </a>
+          </span>
+        </header>
+      </div>
+    </div>
+  );
+};
